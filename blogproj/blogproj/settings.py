e5c4cd34869dd3897d00bab37a29cf1c290763b0
AKUZWE-Sheja <edwigeakuzwe6@gmail.com>
2024-09-28 18:17:53 +0200

    Register done

--- blogproj/blogproj/settings.py
+++ blogproj/blogproj/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     # own
 
     'blog.apps.BlogConfig',  
+    'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
